9e8aadcaeb9bdcae6cd5c2fb41e22cbd5e425d18
JackYan <958212290@qq.com>
2018-04-25 00:08:01 +0800

    0425 update
    0425 update

--- 3.data handler/handler.py
+++ 3.data handler/handler.py
@@ -1,1 +1,24 @@
-with open ()
+#coding=utf-8
+import os
+
+os.chdir(os.getcwd()+'\data')
+
+with open('james.txt') as jaf:
+    data = jaf.readline()
+james=data.strip().split(',')
+print(sorted(james))
+
+with open('julie.txt') as juf:
+    data = juf.readline()
+julie=data.strip().split(',')
+print(sorted(julie))
+
+with open('mikey.txt') as mif:
+    data = mif.readline()
+mikey=data.strip().split(',')
+print(sorted(mikey))
+
+with open('sarah.txt') as saf:
+    data = saf.readline()
+sarah=data.strip().split(',')
+print(sorted(sarah))
